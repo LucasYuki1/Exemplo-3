990ac266244dd8f21786845f84191d215d13d0e3
Lucas <lucasenzoideyuki@gmail.com>
2024-08-26 09:05:39 -0300

    Criando as rotas dos componentes Home, Aluno e Erro

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Aluno from './routes/Aluno.jsx'
+import Home from './routes/Home.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+{/* Criando a função createBrowserRouter */}
+
+const router = createBrowserRouter([
+  {
+    // Chamando os elementos Pai
+    path:'/', element: <App/>,
+    errorElement:<Error/>,
+
+    // Chamando os elementos filhos
+    children:[
+      // Criando a rota home
+      {path:'/', element:<Home/>},
+      // Criando a rota aluno
+      {path:'/aluno', element:<Aluno/>}
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Criando o props que será renderizado */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/routes/Aluno.jsx
@@ -0,0 +1,8 @@
+const Aluno = () => {
+    return(
+        <>
+            <h1>Página Aluno</h1>
+        </>
+    )
+}
+export default Aluno
